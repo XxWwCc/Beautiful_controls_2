255b0b1a41c26a938616162785869b2a2a85e46b
smoker4 <somker4@qi-cloud.com>
2019-01-22 13:52:02 +0800

    在dialogTest中添加测试使用Picasso加载图片框架

--- app/src/main/java/com/qcloud/myapplication/activity/DialogTest.kt
+++ app/src/main/java/com/qcloud/myapplication/activity/DialogTest.kt
@@ -1,5 +1,6 @@
 package com.qcloud.myapplication.activity
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.ActivityOptions
 import android.content.Context
@@ -16,6 +17,8 @@ import kotlinx.android.synthetic.main.activity_dialog_test.*
 import java.util.concurrent.TimeUnit
 import com.codetroopers.betterpickers.calendardatepicker.CalendarDatePickerDialogFragment
 import android.widget.Toast
+import com.squareup.picasso.Picasso
+import jp.wasabeef.picasso.transformations.CropCircleTransformation
 import java.util.*
 
 
@@ -26,6 +29,7 @@ import java.util.*
  */
 class DialogTest : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dialog_test)
@@ -52,6 +56,8 @@ class DialogTest : AppCompatActivity() {
             val cdp = CalendarDatePickerDialogFragment()
                 .setOnDateSetListener { _, year, monthOfYear, dayOfMonth ->
                     Toast.makeText(this, "$year-${fillZero(monthOfYear+1)}-${fillZero(dayOfMonth)}", Toast.LENGTH_LONG).show()
+                    tv_load_success.text = "$year-${fillZero(monthOfYear+1)}-${fillZero(dayOfMonth)}"
+                    loadImage()
                 }
                 .setFirstDayOfWeek(Calendar.SUNDAY)
                 .setPreselectedDate(year, month, day)
@@ -59,6 +65,21 @@ class DialogTest : AppCompatActivity() {
                 .setCancelText("取消")
             cdp.show(supportFragmentManager, "")
         }
+    }
+
+    private fun loadImage() {
+        val url = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548136930551&di=182c19e3bda4f8436370e0b96ad71487&imgtype=0&src=http%3A%2F%2Fpic.kekenet.com%2F2015%2F0727%2F57891437981090.gif"
+        // picasso无法加载gif动态图，使用只会是静图
+        Picasso.with(this)
+            .load(url)
+            .placeholder(R.mipmap.icon_no_network)      // 加载未完成显示图片
+            .error(R.mipmap.icon_no_data)               // 加载出错图片
+            .resize(800, 600)     /*重设宽高 可用resizeDimen（）代替，使用 dimen.xml
+                                                 fit()方法可以不用计算大小调用resize，他直接把图片裁剪成imageview的大小*/
+            .centerInside()                             // 宽或高已经到边缘时，不进行拉伸变形
+            .rotate(90f)                   /*以（0，0）为中心旋转180度，可以使用rotate(degrees, x, y)指定旋转中心*/
+            .transform(CropCircleTransformation())  // 使用的是一个专门支持picasso的框架
+            .into(iv_congratulations)
     }
 
     /**

8cbb8dfe60c55b10966a1bbf4663b9bbba658248
smoker4 <somker4@qi-cloud.com>
2019-01-23 18:16:30 +0800

    添加通知功能

--- app/src/main/java/com/qcloud/myapplication/activity/MainActivity.kt
+++ app/src/main/java/com/qcloud/myapplication/activity/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.qcloud.myapplication.activity
 
-import android.app.AlertDialog
-import android.app.DatePickerDialog
+import android.Manifest
+import android.app.*
 import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.net.Uri
 import com.qcloud.myapplication.adapter.FruitAdapter
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.support.design.widget.Snackbar
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.NotificationCompat
+import android.support.v4.app.NotificationManagerCompat
+import android.support.v4.content.ContextCompat
 import android.support.v4.view.GravityCompat
 import android.support.v7.widget.GridLayoutManager
+import android.telephony.SmsManager
 import android.transition.Slide
 import android.util.DisplayMetrics
 import android.view.Gravity
@@ -19,13 +31,17 @@ import android.widget.Toast
 import com.qcloud.myapplication.beans.FruitBean
 import com.qcloud.myapplication.R
 import com.qcloud.myapplication.weight.CustomToolbar
+import com.qcloud.myapplication.weight.PermissionManager
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 import java.util.*
 import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     private val fruits: MutableList<FruitBean> = ArrayList()
+
+    private val code = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -62,14 +78,17 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_telephone -> {
                     getDate()
                 }
+                R.id.nav_time -> {
+
+                }
             }
             drawer_layout.closeDrawers()
             return@setNavigationItemSelectedListener true
         }
         fab.setOnClickListener{
-            view ->  Snackbar.make(view, "Data deleted", Snackbar.LENGTH_SHORT)
-                            .setAction("Undo", View.OnClickListener {
-                                Toast.makeText(this, "Data restored", Toast.LENGTH_SHORT).show()
+            view ->  Snackbar.make(view, "Send Notice", Snackbar.LENGTH_SHORT)
+                            .setAction("Yes", View.OnClickListener {
+                                checkPermissions()
                             })
                             .show()
         }
@@ -82,6 +101,99 @@ class MainActivity : AppCompatActivity() {
         swipe_refresh.setOnRefreshListener {
             refreshFruits()
         }
+    }
+
+    /**
+     * 检查权限
+     * */
+    private fun checkPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkIs360Rom() && !NotificationManagerCompat.from(this).areNotificationsEnabled()) {
+                //判断是否是360手机并且是否打开通知功能
+                requestToSetting()
+            } else {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, Array<String>(1) { Manifest.permission.SEND_SMS }, code)
+                } else {
+                    sendNotice()
+                }
+            }
+        } else {
+            sendNotice()
+        }
+    }
+
+    /**
+     * 检查是否360系统手机
+     * */
+    fun checkIs360Rom(): Boolean {
+        // fix issue https://github.com/zhaozepeng/FloatWindowPermission/issues/9
+        return Build.MANUFACTURER.contains("QiKU") || Build.MANUFACTURER.contains("360")
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            code -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    sendNotice()
+                } else {
+                    Toast.makeText(this, "发送短信的权限已被拒绝，无法发送短信", Toast.LENGTH_SHORT).show()
+                }
+                if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)) {
+                    requestToSetting()
+                } else {
+                    Toast.makeText(this, "请去设置中打开发送短信的权限，否则无法发送短信", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    private fun requestToSetting() {
+        val record = AlertDialog.Builder(this)
+            .setNegativeButton("取消"){_,_ -> }
+            .setPositiveButton("确定") { _, _ ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivityForResult(intent, 2)
+            }
+        if (checkIs360Rom()) {
+            record.setMessage("请开启发送通知功能")
+        } else {
+            record.setMessage("请开启发送短信功能")
+        }
+        record.show()
+    }
+
+    /**
+     * 发送一条消息
+     * */
+    private fun sendNotice() {
+        val CHANNEL_ID = "channel_id_1"
+        val CHANNEL_NAME = "channel_name_1"
+        val intent = Intent(this, BannerTestActivity::class.java)
+        val pi = PendingIntent.getActivity(this, 0, intent, 0)
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+            manager.createNotificationChannel(notificationChannel)
+        }
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("This is SmokerApp notice title")
+            .setContentText("This is content text")
+            .setWhen(System.currentTimeMillis())
+            .setSmallIcon(R.mipmap.smoker)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
+            .setContentIntent(pi)
+            .setAutoCancel(true)
+//            .setSound(Uri.fromFile(File("/system/media/audio/ringtones/luna.ogg")))
+//            .setVibrate(longArrayOf(0, 1000, 1000, 1000))
+//            .setLights(Color.GREEN, 1000, 1000)
+            .setDefaults(NotificationCompat.DEFAULT_ALL) //默认的
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .build()
+        manager.notify(1,notification)
     }
 
     /**

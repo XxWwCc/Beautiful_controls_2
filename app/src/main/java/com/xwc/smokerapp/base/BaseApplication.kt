d68dc7f2bb964b58066d960c0ec56105a2626232
smoker4 <somker4@qi-cloud.com>
2019-03-28 11:41:00 +0800

    添加签名问价，使用极光推送并使用自己的receiver拿取内容

--- app/src/main/java/com/xwc/smokerapp/base/BaseApplication.kt
+++ app/src/main/java/com/xwc/smokerapp/base/BaseApplication.kt
@@ -1,6 +1,8 @@
 package com.xwc.smokerapp.base
 
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
 import com.xwc.smokerapp.utils.AppManager
 import com.xwc.smokerapp.utils.SharedUtil
 import timber.log.Timber
@@ -24,6 +26,15 @@ class BaseApplication : Application() {
         } else {
             Timber.plant()
         }
+    }
+
+    /**
+     * 解决Android 5.0以下方法超限
+     * 打包apk文件时会提示方法超限
+     * */
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
     }
 
     companion object {
